fcf2b80e5e2e109fd39a67a6c779054ecd250a81
nejatandimeh-beep <62664064+nejatandimeh-beep@users.noreply.github.com>
2021-08-09 20:52:26 +0430

    tanastyle ver 1.1

---
+++ laravel/app/Admin.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App;
+
+use App\Notifications\ResetPassword as ResetPasswordNotification;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Admin extends Authenticatable
+{
+    use Notifiable;
+
+    protected $table = 'admins';
+
+    protected $fillable = [
+        'name', 'email', 'password','family','role'
+    ];
+
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function sendPasswordResetNotification($token)
+    {
+        // Your your own implementation.
+        $this->notify(new ResetPasswordNotification($token));
+    }
+}
