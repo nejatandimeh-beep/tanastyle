ccb64ca41a7202863d54708bee3a7f4944d45ed2
nejatandimeh-beep <62664064+nejatandimeh-beep@users.noreply.github.com>
2021-12-28 20:17:03 +0330

    tanastyle ver 2.3

--- laravel/app/Http/Controllers/AuthSeller/RegisterController.php
+++ laravel/app/Http/Controllers/AuthSeller/RegisterController.php
@@ -160,8 +160,8 @@ class RegisterController extends Controller
             'PicPath' => $request['userImage'],
             'PicPathCard' => $request['nationalityCardImage'],
             'email' => $request['email'],
-            'password' => Hash::make($password),
             'PasswordHint' => $password,
+            'password' => Hash::make($password),
         ]);
 
         $id = DB::table('sellers')
