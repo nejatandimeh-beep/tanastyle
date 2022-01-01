d9076c85294932bd450a8b04a7afe2c7a23a1e8e
nejatandimeh-beep <62664064+nejatandimeh-beep@users.noreply.github.com>
2022-01-01 19:06:12 +0330

    tanastyle ver 2.5

--- laravel/resources/views/Layouts/IndexCustomer.blade.php
+++ laravel/resources/views/Layouts/IndexCustomer.blade.php
@@ -31,6 +31,18 @@
         <script src="{{ asset('assets/js/components/hs.popup.js') }}"></script>
         <script src="{{ asset('assets/js/components/hs.modal-window.js') }}"></script>
     @break
+    @case(strpos($_SERVER['REQUEST_URI'],'/Customer-Product'))
+    <!--تخفیفات ویژه-->
+    <script src="{{ asset('assets/js/components/hs.carousel.js') }}"></script>
+    <script src="{{ asset('assets/vendor/revolution-slider/revolution/js/extensions/revolution.extension.carousel.min.js') }}"></script>
+    <script src="{{ asset('assets/vendor/slick-carousel/slick/slick.js') }}"></script>
+    <!--Modal and cropper-->
+    <script src="{{ asset('assets/js/cropper.js') }}"></script>
+    <script src="{{ asset('assets/vendor/custombox/custombox.min.js') }}"></script>
+    <script src="{{ asset('assets/vendor/malihu-scrollbar/jquery.mCustomScrollbar.concat.min.js') }}"></script>
+    <script src="{{ asset('assets/js/components/hs.popup.js') }}"></script>
+    <script src="{{ asset('assets/js/components/hs.modal-window.js') }}"></script>
+    @break
     @case(strpos($_SERVER['REQUEST_URI'],'/Customer-Profile/'))
         <!--Side Menu-->
         <script src="{{ asset('assets/vendor/revolution-slider/revolution/js/jquery.themepunch.tools.min.js') }}"></script>
