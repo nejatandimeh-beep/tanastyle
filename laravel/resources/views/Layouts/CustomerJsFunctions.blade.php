cb40b3c731b1bee21cfe5fb10c02e3819991354f
nejatandimeh-beep <62664064+nejatandimeh-beep@users.noreply.github.com>
2022-01-13 19:51:42 +0330

    tanastyle ver 2.7

--- laravel/resources/views/Layouts/CustomerJsFunctions.blade.php
+++ laravel/resources/views/Layouts/CustomerJsFunctions.blade.php
@@ -178,32 +178,6 @@
                 });
             }
 
-            if ($('#productGallery').length > 0 && $('#productGallery').hasClass('largeDevice')) {
-                magnify("img1", 3);
-                if ($('#img2').length > 0)
-                    magnify("img2", 3);
-                if ($('#img3').length > 0)
-                    magnify("img3", 3);
-                if ($('#img4').length > 0)
-                    magnify("img4", 3);
-                if ($('#img5').length > 0)
-                    magnify("img5", 3);
-                if ($('#img6').length > 0)
-                    magnify("img6", 3);
-                if ($('#img7').length > 0)
-                    magnify("img7", 3);
-                if ($('#img8').length > 0)
-                    magnify("img8", 3);
-                if ($('#img9').length > 0)
-                    magnify("img9", 3);
-                if ($('#img10').length > 0)
-                    magnify("img10", 3);
-                if ($('#img11').length > 0)
-                    magnify("img11", 3);
-                if ($('#img12').length > 0)
-                    magnify("img12", 3);
-            }
-
             if (!$('#productDiv').length > 0) {
                 $('#noProduct').show();
             }
@@ -313,6 +287,32 @@
             if (state === 'complete') {
                 $(document.body).removeClass('me-position-fix');
                 $(document.body).addClass('me-position-normally');
+                if ($('#productGallery').length > 0 && $('#productGallery').hasClass('largeDevice')) {
+                    magnify("img1", 3);
+                    if ($('#img2').length > 0)
+                        magnify("img2", 3);
+                    if ($('#img3').length > 0)
+                        magnify("img3", 3);
+                    if ($('#img4').length > 0)
+                        magnify("img4", 3);
+                    if ($('#img5').length > 0)
+                        magnify("img5", 3);
+                    if ($('#img6').length > 0)
+                        magnify("img6", 3);
+                    if ($('#img7').length > 0)
+                        magnify("img7", 3);
+                    if ($('#img8').length > 0)
+                        magnify("img8", 3);
+                    if ($('#img9').length > 0)
+                        magnify("img9", 3);
+                    if ($('#img10').length > 0)
+                        magnify("img10", 3);
+                    if ($('#img11').length > 0)
+                        magnify("img11", 3);
+                    if ($('#img12').length > 0)
+                        magnify("img12", 3);
+                }
+
             }
         }
 
