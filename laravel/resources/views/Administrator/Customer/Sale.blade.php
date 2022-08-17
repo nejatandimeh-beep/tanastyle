e6e52fca8e5626c0e5f796fe7f84378678292733
nejatandimeh-beep <62664064+nejatandimeh-beep@users.noreply.github.com>
2022-08-17 19:40:42 +0430

    اپدیت ورژن قبل

--- laravel/resources/views/Administrator/Customer/Sale.blade.php
+++ laravel/resources/views/Administrator/Customer/Sale.blade.php
@@ -463,7 +463,6 @@
                                         <th class="align-middle text-center text-nowrap">مبلغ فاکتور<span
                                                 class="g-font-size-10 g-mr-3">(تومان)</span>
                                         </th>
-                                        <th class="align-middle text-center">هزینه پستی</th>
                                         <th class="align-middle text-center">مجموع</th>
                                         <th class="align-middle text-center">تاریخ</th>
                                         <th class="align-middle text-center text-nowrap">شماره فاکتور</th>
@@ -497,7 +496,6 @@
                                             <td class="align-middle text-center">{{ $rec->Qty }}</td>
                                             <td class="align-middle text-center">{{ number_format($rec->FinalPrice * $rec->Qty) }}</td>
                                             <td class="align-middle text-center text-nowrap">{{ number_format($rec->PostPrice) }}</td>
-                                            <td class="align-middle text-center text-nowrap">{{ number_format($rec->FinalPrice * $rec->Qty+$rec->PostPrice) }}</td>
                                             <td class="align-middle text-center text-nowrap">{{ $persianDate[$key][0].'/'.$persianDate[$key][1].'/'.$persianDate[$key][2] }}</td>
                                             <td class="align-middle text-center">{{ $rec->orderID.'/'.$rec->orderDetailID }}</td>
                                             <td class="align-middle text-center text-nowrap">{{ $rec->PostMethod }}</td>
